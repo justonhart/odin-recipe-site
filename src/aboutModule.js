74eb76b4c56f7f699307b069ccec704a6fc964b8
Juston Hart <contact@justonh.art>
2024-05-30 12:02:00 -0500

    Add simple About page, clamp content width

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -1,3 +1,12 @@
 export default function renderAbout(content){
-	content.innerHTML = 'about';
+	//clear content
+	content.innerHTML = '';
+
+	const header = document.createElement('h2');
+	header.innerText = "About this site";
+	content.appendChild(header);
+
+	const para = document.createElement('p');
+	para.innerText = 'This site was created as part of the Odin Project, a self-driven bootcamp for web development. The images used for this site are all royalty-free images pulled from Pixabay.com. For more information about this project, the Odin Project, or my other projects, send me an email at contact@justonh.art.';
+	content.appendChild(para);
 }
--- src/menuModule.js
+++ src/menuModule.js
@@ -1,3 +1,45 @@
 export default function renderMenu(content){
-	content.innerHTML = 'menu';
+	//reset content
+	content.innerHTML = '';
+
+	//add menu header
+	const header = document.createElement('h2');
+	header.innerText = 'Our Menu';
+	content.appendChild(header);
+	
+	//add menu card
+	const menuCard = document.createElement('div');
+	menuCard.classList.add('mainCard');
+	menuCard.classList.add('flexColumn');
+	menuCard.classList.add('shadowed');
+	content.appendChild(menuCard);
+
+	//create menu section
+	const entreeSection = document.createElement('div');
+	entreeSection.classList.add('menuSection');
+	menuCard.appendChild(entreeSection);
+
+	const entreeSectionHeader = document.createElement('h3');
+	entreeSectionHeader.innerText = 'Entrees';
+	entreeSection.appendChild(entreeSectionHeader);
+
+	//create menu items
+	entreeSection.appendChild(createMenuRow('Hipster Meat', 60));
+	entreeSection.appendChild(createMenuRow('Organic Spaghetti', 35));
+	entreeSection.appendChild(createMenuRow('Alley-grown Berries', 19));
+}
+
+function createMenuRow(item, price){
+	const menuRow = document.createElement('div');
+	menuRow.classList.add('menuRow');
+
+	const itemSpan = document.createElement('span');
+	itemSpan.innerText = item;
+	menuRow.appendChild(itemSpan);
+
+	const priceSpan = document.createElement('span');
+	priceSpan.innerText = price;
+	menuRow.appendChild(priceSpan);
+
+	return menuRow;
 }
